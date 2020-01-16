551f7c03e7080a318ce01a65e658112deb788fab
Mathias Lavaert <mlavaert@gmail.com>
2020-01-16 17:20:32 +0100

    More cleanup

--- .bashrc.d/.functions.bash
+++ .bashrc.d/.functions.bash
@@ -7,15 +7,6 @@ function path() {
     echo $PATH | tr ':' '\n'
 }
 
-
-function awscat() {
-    aws s3 cp $1 -
-}
-
-function generate-password() {
-    echo $(openssl rand -base64 ${1:-12})
-}
-
 function aws-credentials-for-docker() {
     echo "-e AWS_ACCESS_KEY_ID=`aws configure get aws_access_key_id` " \
          "-e AWS_SECRET_ACCESS_KEY=`aws configure get aws_secret_access_key` " \
