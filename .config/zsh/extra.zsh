88491f78ca979359b0b68d1d39f0f744f354a759
Mathias Lavaert <mlavaert@gmail.com>
2020-05-11 08:49:04 +0200

    extra

--- .config/zsh/extra.zsh
+++ .config/zsh/extra.zsh
@@ -3,18 +3,39 @@ typeset -gU path fpath mailpath cdpath
 path=($XDG_BIN_HOME $HOME/.bin $HOME/.node_modules/bin $path)
 fpath=($XDG_BIN_HOME $fpath)
 
+function _is_interactive { [[ $- == *i* ]]; }
+function _cache {
+  _is_interactive || return 1
+  local cache_dir="$XDG_CACHE_HOME/${SHELL##*/}"
+  local cache="$cache_dir/$1"
+  if [[ ! -f $cache || ! -s $cache ]]; then
+    echo "Caching $1"
+    mkdir -p $cache_dir
+    "$@" >$cache
+  fi
+  source $cache
+}
+
+
 # Python
 export PIP_REQUIRE_VIRTUALENV=true
 export PIP_DOWNLOAD_CACHE=$XDG_CACHE_HOME/pip
 export IPYTHONDIR=$XDG_CONFIG_HOME/jupyter
 export JUPYTER_CONFIG_DIR=$XDG_CONFIG_HOME/jupyter
 
-source $XDG_BIN_HOME/virtualenvwrapper_lazy.sh
+export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
+path=( $PYENV_ROOT/bin $path )
+
+eval "$(pyenv init - --no-rehash)"
+eval "$(pyenv virtualenv-init -)"
 
 # Java
 export JAVA_HOME="/usr/lib/jvm/default"
 export GRADLE_USER_HOME="$XDG_CACHE_HOME/gradle"
 
+# Golang
+export GOPATH=$HOME/dev/go
+export PATH=$PATH:$GOPATH/bin
 
 # Aws
 export AWS_PROFILE=masl
