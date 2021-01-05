c868f232989c77a2e31cd8ea00cf68137c41b07c
Mathias Lavaert <mlavaert@gmail.com>
2021-01-05 09:51:45 +0100

    Nordify

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -26,7 +26,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-nord)
 
 ;; org-mode settings
 (setq org-directory "~/org/"
