7b2f897d6267e6aed06de0229a6f7204b2384bd0
Mathias Lavaert <mlavaert@gmail.com>
2021-10-03 21:52:47 +0200

    Userconfig

---
+++ .config/zsh/userconfig.zsh
@@ -0,0 +1,63 @@
+# dotfiles
+alias config='git --git-dir=/home/mlavaert/.dotfiles/ --work-tree=/home/mlavaert'
+
+# radio
+alias willy="mpv https://playerservices.streamtheworld.com/api/livestream-redirect/WILLYAAC.AAC"
+
+alias vim=nvim
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias -- -='cd -'
+alias ll='ls -laF --color'
+alias ls='ls --color'
+
+alias mkdir='mkdir -p'
+
+# aliases for tools
+alias mk=make
+alias tf=terraform
+alias k=kubectl
+
+alias y='xclip -selection clipboard -in'
+alias p='xclip -selection clipboard -out'
+
+
+# environment
+export PIP_REQUIRE_VIRTUALENV=true
+
+# Java
+export JAVA_HOME="/usr/lib/jvm/default"
+
+# Aws
+export AWS_PROFILE=masl
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_SDK_LOAD_CONFIG=1
+
+# XDG Specification
+export PIP_DOWNLOAD_CACHE=$XDG_CACHE_HOME/pip
+export IPYTHONDIR=$XDG_CONFIG_HOME/jupyter
+export JUPYTER_CONFIG_DIR=$XDG_CONFIG_HOME/jupyter
+export LESSKEY=$XDG_CONFIG_HOME/less/lesskey
+export LESSHISTFILE=$XDG_CACHE_HOME/less/history
+export DOCKER_CONFIG=$XDG_CONFIG_HOME/docker
+
+# Utility functions
+function bw-unlock() {
+    export BW_SESSION=$(bw unlock --raw)
+}
+
+function gpip() {
+    PIP_REQUIRE_VIRTUALENV=false pip "$@"
+}
+
+function kcluster() {
+    local cluster=`aws eks list-clusters --output text | awk '{print $2}' | fzf`
+    aws eks update-kubeconfig --name "$cluster"
+}
+
+function kssh() {
+    local pod=`kubectl get pods "$@" | awk '/Running/ {print $1}' | fzf`
+    kubectl exec -it "$@" ${pod} bash
+}
