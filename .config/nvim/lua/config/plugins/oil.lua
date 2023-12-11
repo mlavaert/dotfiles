dd27d0158c721a3a217175f94b04ea17bb3c56c2
Mathias Lavaert <mathias@pilcrow.be>
2023-12-11 09:56:42 +0100

    Added plugins

---
+++ .config/nvim/lua/config/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/oil.nvim",
+  config = true,
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+
+  keys = { { "-", "<cmd>Oil<cr>", desc = "Oil" } },
+}
